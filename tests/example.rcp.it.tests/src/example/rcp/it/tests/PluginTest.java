bc5e02c30edb4e519419e29844f162127608ef15
Vodorok <gabor.kutas@ericsson.com>
2019-02-19 10:49:20 +0100

    travis swtbot fix

--- tests/example.rcp.it.tests/src/example/rcp/it/tests/PluginTest.java
+++ tests/example.rcp.it.tests/src/example/rcp/it/tests/PluginTest.java
@@ -4,27 +4,37 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.not;
 
 import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.results.VoidResult;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.ui.PlatformUI;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 public class PluginTest {
 
-    private static SWTBot bot;
+	private static SWTBot bot;
 
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        // don't use SWTWorkbenchBot here which relies on Platform 3.x
-        bot = new SWTBot();
-    }
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		// don't use SWTWorkbenchBot here which relies on Platform 3.x
+		bot = new SWTBot();
+		UIThreadRunnable.syncExec(new VoidResult() {
+			public void run() {
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().forceActive();
+			}
+		});
+	}
 
-    @Test
-    public void ensureSaveIsDisabledWhenNothingIsDirty() {
-        SWTBotMenu menu = bot.menu("File").menu("Save");
+	@Test
+	public void ensureSaveIsDisabledWhenNothingIsDirty() {
 
-        assertThat("Save command in menu is not enabled", not(menu.isEnabled()));
-    }
+		SWTBotMenu menu = bot.menu("File").menu("Save");
+
+		assertThat("Save command in menu is not enabled", not(menu.isEnabled()));
+	}
 
 }
